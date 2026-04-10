eff10b8745ec02fa8aa73b7674023713ca15a8da
Sabareesh001 <sabareeshcomp@gmail.com>
2026-04-10 21:34:24 +0530

    task-1 update

---
+++ task-1/main.cs
@@ -0,0 +1,22 @@
+﻿var a = Console.ReadLine();
+if (long.TryParse(a, out long i))
+{
+    long factorial = 1;
+    for (long j = 2; j <= i; j++)
+    {
+        try
+        {
+            factorial = checked(factorial * j);
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Result too large to calculate");
+            return;
+        }
+    }
+    Console.WriteLine($"The factorial of {i} is {factorial}");
+}
+else
+{
+    Console.WriteLine("Invalid Input");
+}
