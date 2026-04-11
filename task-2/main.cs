dc88acfd305097e7b44b8d2b053c8f11b47ca01d
Sabareesh001 <sabareeshcomp@gmail.com>
2026-04-11 11:38:36 +0530

    task-2 update

---
+++ task-2/main.cs
@@ -0,0 +1,21 @@
+﻿
+public static class MainClass
+{
+    private class Person(string Name, int age)
+    {
+        public void Introduce()
+        {
+            Console.WriteLine($"Hi, I am {Name} and I am {age} years old");
+        }
+    }
+    public static void Main()
+    {
+        var p1 = new Person("Ram", 25);
+        var p2 = new Person("Alice", 20);
+
+        p1.Introduce();
+        p2.Introduce();
+        
+    }
+}
+
